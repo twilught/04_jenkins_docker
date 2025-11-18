dbf5622d48d22d2c4672176b25d78972200c9cc2
twilught <kongba5555@gmail.com>
2025-11-19 02:24:56 +0700

    ..

--- 01_api/index.js
+++ 01_api/index.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'dit312_6090059',
+  database: process.env.DB_NAME || 'dit312_6605086',
   port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 10,
